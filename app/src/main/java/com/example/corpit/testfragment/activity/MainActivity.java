7aa7910ea44b921b907eef7f0551adea102827c7
zengxianhan <zengxianhan@corpit.com.sg>
2017-07-12 14:41:36 +0800

    Update once

--- app/src/main/java/com/example/corpit/testfragment/activity/MainActivity.java
+++ app/src/main/java/com/example/corpit/testfragment/activity/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.corpit.testfragment.activity;
 
+import android.graphics.Color;
+import android.os.RemoteException;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.content.Context;
@@ -8,6 +10,7 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
@@ -16,7 +19,17 @@ import android.widget.LinearLayout;
 import com.example.corpit.testfragment.R;
 import com.example.corpit.testfragment.adapter.MainViewPagerAdapter;
 
-public class MainActivity extends FragmentActivity {
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.MonitorNotifier;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+
+import java.util.Collection;
+
+public class MainActivity extends FragmentActivity implements BeaconConsumer {
     private LinearLayout mToMainPage;
     private LinearLayout mToSecondPage;
     private LinearLayout mToThirdPage;
@@ -30,10 +43,23 @@ public class MainActivity extends FragmentActivity {
     private ViewPager viewPager;
     private MainViewPagerAdapter adapter;
 
+    private BeaconManager beaconManager;
+    public static final String FILTER_UUID = "E2C56DB5-DFFB-48D2-B060-D0F5A71096E0";
+    public static final String IBEACON_FORMAT ="m:2-3=0x0215,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25";
+    protected static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        beaconManager = BeaconManager.getInstanceForApplication(getApplicationContext());
+        // To detect proprietary beacons, you must add a line like below corresponding to your beacon
+        // type.  Do a web search for "setBeaconLayout" to get the proper expression.
+//        beaconManager.getBeaconParsers().add(new BeaconParser().
+//                setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
+        beaconManager.getBeaconParsers().add(new BeaconParser().
+                setBeaconLayout(IBEACON_FORMAT));
+        beaconManager.bind(this);
         initView();
         initData();
         initEvent();
@@ -60,8 +86,8 @@ public class MainActivity extends FragmentActivity {
         adapter = new MainViewPagerAdapter(fragmentManager);  //定义新FragmentPagerAdapter
         viewPager.setAdapter(adapter);
 
-        int colorPressed = R.color.colorTitle;  //下方Tag按钮的显示颜色
-        imageMainPage.setBackgroundResource(colorPressed);
+        int colorPressed = R.color.color_dark_gray;  //下方Tag按钮的显示颜色
+        mToMainPage.setBackgroundResource(colorPressed);
     }
 
     //初始化控件事件
@@ -81,23 +107,23 @@ public class MainActivity extends FragmentActivity {
 
             @Override
             public void onPageSelected(int position) {
-                int colorPressed = R.color.colorTitle;
+                int colorPressed = R.color.color_dark_gray;
                 switch (position) {
                     case 0:
                         resetBtnColorStatus();
-                        imageMainPage.setBackgroundResource(colorPressed);
+                        mToMainPage.setBackgroundResource(colorPressed);
                         break;
                     case 1:
                         resetBtnColorStatus();
-                        imageSecondPage.setBackgroundResource(colorPressed);
+                        mToSecondPage.setBackgroundResource(colorPressed);
                         break;
                     case 2:
                         resetBtnColorStatus();
-                        imageThirdPage.setBackgroundResource(colorPressed);
+                        mToThirdPage.setBackgroundResource(colorPressed);
                         break;
                     case 3:
                         resetBtnColorStatus();
-                        imageFourthPage.setBackgroundResource(colorPressed);
+                        mToFourthPage.setBackgroundResource(colorPressed);
                         break;
                 }
             }
@@ -114,27 +140,28 @@ public class MainActivity extends FragmentActivity {
 
         @Override
         public void onClick(View v) {
-            int colorPressed = R.color.colorTitle;
+            int colorPressed = R.color.color_dark_gray;
             switch (v.getId()) {
                 case R.id.id_ll_tab_main_page:
                     goToMainPageFragment();
                     resetBtnColorStatus();
-                    imageMainPage.setBackgroundResource(colorPressed);
+                    mToMainPage.setBackgroundResource(colorPressed);
                     break;
                 case R.id.id_ll_tab_two_page:
                     goToSecondPageFragment();
                     resetBtnColorStatus();
-                    imageSecondPage.setBackgroundResource(colorPressed);
+
+                    mToSecondPage.setBackgroundResource(colorPressed);
                     break;
                 case R.id.id_ll_tab_third_page:
                     goToThirdPageFragment();
                     resetBtnColorStatus();
-                    imageThirdPage.setBackgroundResource(colorPressed);
+                    mToThirdPage.setBackgroundResource(colorPressed);
                     break;
                 case R.id.id_ll_tab_fourth_page:
                     goToFourthPageFragment();
                     resetBtnColorStatus();
-                    imageFourthPage.setBackgroundResource(colorPressed);
+                    mToFourthPage.setBackgroundResource(colorPressed);
                     break;
             }
         }
@@ -164,9 +191,88 @@ public class MainActivity extends FragmentActivity {
     //重置所有的按钮颜色
     private void resetBtnColorStatus() {
         int colorReset = R.color.colorNoColor;
-        imageMainPage.setBackgroundResource(colorReset);
-        imageSecondPage.setBackgroundResource(colorReset);
-        imageThirdPage.setBackgroundResource(colorReset);
-        imageFourthPage.setBackgroundResource(colorReset);
+        mToMainPage.setBackgroundResource(colorReset);
+        mToSecondPage.setBackgroundResource(colorReset);
+        mToThirdPage.setBackgroundResource(colorReset);
+        mToFourthPage.setBackgroundResource(colorReset);
+    }
+
+    @Override
+    public void onBeaconServiceConnect() {
+        Log.d(TAG, "connectedService");
+        beaconManager.addMonitorNotifier(new MonitorNotifier() {
+            @Override
+            public void didEnterRegion(Region region) {
+                Log.i(TAG, "I just saw an beacon for the first time!");
+            }
+
+            @Override
+            public void didExitRegion(Region region) {
+                Log.i(TAG, "I no longer see an beacon");
+            }
+
+            @Override
+            public void didDetermineStateForRegion(int i, Region region) {
+                Log.i(TAG, "I have just switched from seeing/not seeing beacons: " + i);
+            }
+        });
+        try {
+            beaconManager.startMonitoringBeaconsInRegion(new Region("myMonitoringUniqueId", null, null, null));
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+
+        beaconManager.addRangeNotifier(new RangeNotifier() {
+            @Override
+            public void didRangeBeaconsInRegion(Collection<Beacon> collection, Region region) {
+                Log.i("beacon size",  "beacon size = " + collection.size());
+                if (collection.size() > 0) {
+                    for (Beacon b :
+                            collection) {
+                        Log.i(TAG, "The "+ b.getBluetoothName() + " beacon I see about " + b.getDistance() + " meters away.");
+                        Log.d("beaconTest", "distance: " +
+                                b.getDistance() + "id: " +
+                                b.getId1() + "/" +
+                                b.getId2() + "/" +
+                                b.getId3());
+                    }
+
+                }
+            }
+        });
+
+        try {
+            //开始监视
+            beaconManager.startMonitoringBeaconsInRegion(new Region(FILTER_UUID, null, null, null));
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        beaconManager = BeaconManager.getInstanceForApplication(getApplicationContext());
+        // To detect proprietary beacons, you must add a line like below corresponding to your beacon
+        // type.  Do a web search for "setBeaconLayout" to get the proper expression.
+//        beaconManager.getBeaconParsers().add(new BeaconParser().
+//                setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
+        beaconManager.bind(this);
+    }
+
+    public static int colorSchemeConvert(String color){
+        try {
+            String alpha = color.substring(color.indexOf("/")+1,color.lastIndexOf(color)).trim();
+            Integer alphaInt = Math.round( Float.valueOf(alpha) * 255);
+            alpha = Integer.toHexString(alphaInt);
+
+            String rgb = color.substring(0,5);
+            String combineColor = "#" + alpha + rgb;
+            int colorReturn = Color.parseColor(combineColor);
+            return colorReturn;
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            return Color.WHITE;
+        }
     }
 }
--- app/src/main/java/com/example/corpit/testfragment/fragment/SecondPageFragment.java
+++ app/src/main/java/com/example/corpit/testfragment/fragment/SecondPageFragment.java
@@ -1,22 +1,41 @@
 package com.example.corpit.testfragment.fragment;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.RemoteException;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.corpit.testfragment.R;
 
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.BeaconTransmitter;
+import org.altbeacon.beacon.MonitorNotifier;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+
+import java.util.Arrays;
+import java.util.Collection;
+
 /**
  * Created by corpit on 3/4/2017.
  */
 
-public class SecondPageFragment extends Fragment {
+public class SecondPageFragment extends Fragment{
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_page_two,container,false);
+        return inflater.inflate(R.layout.fragment_page_two, container, false);
     }
 }
